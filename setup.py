3ec758fcad5b186fae1a8d95e59948e4faa7cfd2
mattiasoldani <62517661+mattiasoldani@users.noreply.github.com>
2020-05-22 01:18:04 +0200

    preparing space 4 dependencies (install_requires)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[
-        'numpy',
+        "numpy",
     ]
     license="MIT",
     classifiers=[
