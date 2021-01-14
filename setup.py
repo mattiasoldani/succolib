9d823d5ba9f44c335c7ed8af183a2d5a302e4943
Mattia Soldani <mattiasoldani93@gmail.com>
2021-01-14 12:14:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description="This is **succolib**, a library of handy Python functions for High-Energy Physics beamtests data analysis. In particular, it has been developed with a focus on the event-by-event analysis of the data collected with the INSULAb detectors &mdash; see, for example, the experimental configurations described [here](http://cds.cern.ch/record/2672249), [here](http://hdl.handle.net/10277/857) and [here](http://cds.cern.ch/record/1353904). Details can be found in the [succolib GitHub page](https://github.com/mattiasoldani/succolib).",
     long_description_content_type="text/markdown",
     packages=find_packages(),
-    python_requires=">3.0",
+    python_requires=">=3.0",
     install_requires=[
         "numpy",
         "pandas",
